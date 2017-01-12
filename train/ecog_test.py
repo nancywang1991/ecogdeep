f2b0f4302c0c833e34aec8ba78d315b36dcc1e1e
Ubuntu <wangnxr@gpucluster001.nzjvwr4qmc0edidzuafhfhiekf.jx.internal.cloudapp.net>
2017-01-12 22:45:29 +0000

    3d changes

--- train/ecog_test.py
+++ train/ecog_test.py
@@ -1,6 +1,6 @@
 import keras
 from keras.applications.vgg16 import VGG16
-from keras.preprocessing.ecog import EcogDataGenerator
+from keras.preprocessing.ecog3D import Ecog3DDataGenerator
 from keras.layers import Flatten, Dense, Input, Dropout, Activation
 from keras.layers.normalization import BatchNormalization
 from keras.models import Model
@@ -10,16 +10,16 @@ from keras.models import Model, load_model
 import numpy as np
 import pdb
 
-test_datagen = EcogDataGenerator(
+test_datagen = Ecog3DDataGenerator(
         center=True
 )
 
 dgdx_val = test_datagen.flow_from_directory(
         #'/mnt/cb46fd46_5_no_offset/test/',
-        '/home/nancy/Documents/ecog_dataset/d6532718/test/',
+        '/mnt/ecog_dataset/d6532718/test/',
         batch_size=24,
         shuffle=False,
-        target_size=(64,1000,1),
+        target_size=(8,8,1000,1),
         class_mode='binary')
 
 #train_datagen.fit_generator(dgdx, nb_iter=100)
@@ -30,7 +30,7 @@ validation_generator=dgdx_val
 #base_model = VGG16(input_tensor=(Input(shape=(224, 224, 3))), include_top=False)
 
 
-model = load_model("ecog_model_1d.h5")
+model = load_model("my_model_ecog3D.h5")
 #pdb.set_trace()
 files = validation_generator.filenames
 results = model.predict_generator(validation_generator, validation_generator.nb_sample)
@@ -46,11 +46,15 @@ for r, result in enumerate(results):
 
 recall = true_1/float(len(np.where(true==1)[0]))
 precision = true_1/float((true_1 + (len(np.where(true==0)[0])-true_0)))
+accuracy_1 = true_1/float(sum(true))
+accuracy_0 = true_0/float((len(np.where(true==0)[0])))
 
-
-with open("ecog_1d_results.txt", "wb") as writer:
+with open("ecog_3d_results.txt", "wb") as writer:
         writer.write("recall:%f\n" % recall)
         writer.write("precision:%f\n" % precision)
+        writer.write("accuracy_1:%f\n" % accuracy_1)
+        writer.write("accuracy_0:%f\n" % accuracy_0)
+
         for f, file in enumerate(files):
                 writer.write("%s:%f\n" % (file, results[f][0]))
 
--- train/ecog_train_3d.py
+++ train/ecog_train_3d.py
@@ -8,6 +8,8 @@ from keras.layers import Convolution3D, MaxPooling3D
 #from keras.imagenet_utils import decode_predictions, preprocess_input, _obtain_input_shape
 import numpy as np
 import pdb
+import pickle
+
 train_datagen = Ecog3DDataGenerator(
         time_shift_range=200,
         gaussian_noise_range=0.001,
@@ -20,15 +22,15 @@ test_datagen = Ecog3DDataGenerator(
 
 
 dgdx = train_datagen.flow_from_directory(
-        #'/mnt/cb46fd46_5_no_offset/train/',
-        '/home/nancy/Documents/ecog_dataset/d6532718/train/',
-        batch_size=24,
+        '/mnt/ecog_dataset/d6532718/train/',
+        #'/home/nancy/Documents/ecog_dataset/d6532718/train/',
+        batch_size=25,
         target_size=(8,8,1000,1),
         class_mode='binary')
 
 dgdx_val = test_datagen.flow_from_directory(
-        #'/mnt/cb46fd46_5_no_offset/test/',
-        '/home/nancy/Documents/ecog_dataset/d6532718/test/',
+        '/mnt/ecog_dataset/d6532718/test/',
+        #'/home/nancy/Documents/ecog_dataset/d6532718/test/',
         batch_size=24,
         shuffle=False,
         target_size=(8,8,1000,1),
@@ -55,19 +57,19 @@ x = MaxPooling3D((1,1,2),  name='block1_pool')(x)
 # Block 2
 x = Convolution3D(64, 2, 2, 10, activation='relu', border_mode='same', name='block2_conv1')(x)
 #x = Convolution2D(64, 1, 10, activation='relu', border_mode='same', name='block2_conv2')(x)
-x = MaxPooling3D((2,2,2),  name='block2_pool')(x)
+x = MaxPooling3D((1,1,2),  name='block2_pool')(x)
 
 # Block 3
 x = Convolution3D(128, 2,2, 10, activation='relu', border_mode='same', name='block3_conv1')(x)
 #x = Convolution2D(128, 1, 10, activation='relu', border_mode='same', name='block3_conv2')(x)
 #x = Convolution2D(128, 1, 10, activation='relu', border_mode='same', name='block3_conv3')(x)
-x = MaxPooling3D((2,2,2),  name='block3_pool')(x)
+x = MaxPooling3D((1,1,2),  name='block3_pool')(x)
 
 # Block 4
 x = Convolution3D(256, 2, 2, 10, activation='relu', border_mode='same', name='block4_conv1')(x)
 #x = Convolution2D(256, 1, 10, activation='relu', border_mode='same', name='block4_conv2')(x)
 #x = Convolution2D(256, 1, 10, activation='relu', border_mode='same', name='block4_conv3')(x)
-x = MaxPooling3D((2,2,2), name='block4_pool')(x)
+x = MaxPooling3D((1,1,2), name='block4_pool')(x)
 
 
 x = Flatten(name='flatten')(x)
@@ -87,19 +89,20 @@ predictions = Activation('sigmoid')(x)
 #    layer.trainable = False
 
 model = Model(input=input_tensor, output=predictions)
-pdb.set_trace()
-sgd = keras.optimizers.SGD(lr=0.01)
+#apdb.set_trace()
+sgd = keras.optimizers.SGD(lr=0.1)
 
 model.compile(optimizer=sgd,
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
+history = keras.callbacks.ModelCheckpoint("/mnt/weights.{epoch:02d}-{val_loss:.2f}.hdf5", save_best_only=True)
 history_callback = model.fit_generator(
         train_generator,
-        samples_per_epoch=24000,
+        samples_per_epoch=7950,
         nb_epoch=100,
         validation_data=validation_generator,
-        nb_val_samples=2400)
+        nb_val_samples=3198, callbacks=[history])
 #pdb.set_trace()
 #loss_history = history_callback.history["loss"]
 #numpy_loss_history = np.array(loss_history)
@@ -108,6 +111,6 @@ with open("loss_history.txt", 'w') as f:
 	for key, value in history_callback.history.items():
 		f.write('%s:%s\n' % (key, value))
 
-model.save("my_model.h5")
-
+model.save("my_model_ecog3D.h5")
+pickle.dump(history_callback.history, open("3d_ecog_history.p", "wb"))
 

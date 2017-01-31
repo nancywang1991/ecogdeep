623dfcec93fdded0e41bcfb72534ae379eafa38d
Nancy Wang <nancywang1991@yahoo.ca>
2017-01-30 18:02:00 -0800

    new ecog test 3d

--- non_deep_algs/ecog_svm.py
+++ non_deep_algs/ecog_svm.py
@@ -33,14 +33,25 @@ dgdx_val = test_datagen.flow_from_directory(
 train_generator=dgdx
 validation_generator=dgdx_val
 samples_per_epoch=20000
+samples_per_epoch_test=validation_generator.nb_sample
 nb_epoch=100
 
 model = SGDClassifier(verbose=1, n_jobs=4)
-test_data = np.vstack([val for val in validation_generator])
+#test_data = np.vstack([val for val in validation_generator])
 for e in xrange(nb_epoch):
     print "Epoch: %i" % e
-    for b in xrange(samples_per_epoch/train_generator.batch_index):
+    mean_test_score = []
+    for b in xrange(samples_per_epoch/train_generator.batch_size):
         data = train_generator.next()
+        if len(data)<train_generator.batch_size:
+            train_generator.reset()
+            data = train_generator.next()
         model.partial_fit(data[0], data[1])
-    model.score(test_data[0], test_data[1])
+    for b in xrange(samples_per_epoch_test/validation_generator.batch_size):
+        test_data = validation_generator.next()
+        if len(test_data)<validation_generator.batch_size:
+            validation_generator.reset()
+            data = validation_generator.next()
+        mean_test_score.append(model.score(test_data[0], test_data[1]))
+    print "Val_acc = %f" % np.mean(np.array(mean_test_score))
 

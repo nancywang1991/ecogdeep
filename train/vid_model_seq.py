1d6d47c6b6513052f8dfcf51a0a5d8e5070b0ece
Nancy Wang <nancywang1991@yahoo.ca>
2017-04-11 02:02:19 -0700

    smth

--- train/vid_model_seq.py
+++ train/vid_model_seq.py
@@ -14,16 +14,16 @@ def vid_model(weights=None):
 
     input_tensor = Input(shape=( 3, 1, 224, 224 ))
     # Block 1
-    x = TimeDistributed(Convolution2D(4, 2, 2, border_mode='same', subsample=(4,4)), name='block1_conv1')(input_tensor)
+    x = TimeDistributed(Convolution2D(48, 11, 11, border_mode='same', subsample=(4,4)), name='block1_conv1')(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
-    x = TimeDistributed(MaxPooling2D((3, 3)),name='block1_pool')(x)
+    x = TimeDistributed(MaxPooling2D((3, 3), strides=(2,2)),name='block1_pool')(x)
 
     # Block 2
-    x = TimeDistributed(Convolution2D(4, 2, 2, border_mode='same'), name='block2_conv1')(input_tensor)
+    x = TimeDistributed(Convolution2D(128, 5, 5, border_mode='same'), name='block2_conv1')(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
-    x = TimeDistributed(MaxPooling2D((3, 3)),name='block2_pool')(x)
+    x = TimeDistributed(MaxPooling2D((3, 3), strides=(2,2)),name='block2_pool')(x)
     # Block 3
     x = TimeDistributed(Convolution2D(192, 3, 3, border_mode='same', name='block3_conv1'))(input_tensor)
     # x = BatchNormalization(axis=1)(x)
@@ -39,8 +39,8 @@ def vid_model(weights=None):
     x = TimeDistributed(MaxPooling2D((3, 3), strides=(2, 2)), name='block5_pool')(x)
 
     x = TimeDistributed(Flatten(),name='flatten')(x)
-    #x = Dropout(0.5)(x)
-    #x = Dense(1024, W_regularizer=l2(0.01), name='fc1')(x)
+    x = Dropout(0.5)(x)
+    x = TimeDistributed(Dense(1024, W_regularizer=l2(0.01)), name='fc1')(x)
     #x = BatchNormalization()(x)
     #x = Activation('relu')(x)
     #x = Dropout(0.5)(x)

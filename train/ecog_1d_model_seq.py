f6e0a7bc46eb86f6646b200dd3c485d40197327d
Nancy Wang <nancywang1991@yahoo.ca>
2017-04-11 02:02:19 -0700

    smth

--- train/ecog_1d_model_seq.py
+++ train/ecog_1d_model_seq.py
@@ -13,13 +13,13 @@ def ecog_1d_model(weights=None, channels=None):
 
     input_tensor = Input(shape=( 3, 1, channels, 200))
     # Block 1
-    x = TimeDistributed(Convolution2D(4, 1, 3, border_mode='same'), name='block1_conv1')(input_tensor)
+    x = TimeDistributed(Convolution2D(16, 1, 3, border_mode='same'), name='block1_conv1')(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
     x = TimeDistributed(MaxPooling2D((1, 3)), name='block1_pool')(x)
 
     # Block 2
-    x = TimeDistributed(Convolution2D(4, 1, 3, border_mode='same'), name='block2_conv1')(x)
+    x = TimeDistributed(Convolution2D(32, 1, 3, border_mode='same'), name='block2_conv1')(x)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
     x = TimeDistributed(MaxPooling2D((1, 3)), name='block2_pool')(x)
@@ -36,8 +36,8 @@ def ecog_1d_model(weights=None, channels=None):
     x = TimeDistributed(MaxPooling2D((1, 3)), name='block4_pool')(x)
 
     x = TimeDistributed(Flatten(), name='flatten')(x)
-    #x = Dropout(0.5)(x)
-    #x = Dense(1024, W_regularizer=l2(0.01), name='fc1')(x)
+    x = Dropout(0.5)(x)
+    x = TimeDistributed(Dense(1024, W_regularizer=l2(0.01)), name='fc1')(x)
     #x = BatchNormalization()(x)
     #x = Activation('relu')(x)
     #x = Dropout(0.5)(x)

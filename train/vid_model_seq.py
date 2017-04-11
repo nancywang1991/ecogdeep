d60b6ed5f9dbc4b00ccd096e737fa8331c2e8e39
Nancy Wang <nancywang1991@yahoo.ca>
2017-04-11 00:11:51 -0700

    smth

--- train/vid_model_seq.py
+++ train/vid_model_seq.py
@@ -10,45 +10,45 @@ import numpy as np
 import pdb
 import pickle
 
-def vid_model(weights=None, channels=None):
+def vid_model(weights=None):
 
-    input_tensor = Input(shape=( 1, 10, 224, 224 ))
+    input_tensor = Input(shape=( 9, 1, 224, 224 ))
     # Block 1
-    x = TimeDistributed(Convolution2D(96, 10, 11, 11, border_mode='same', name='block1_conv1', subsample=(4,4)))(input_tensor)
+    x = TimeDistributed(Convolution2D(96, 11, 11, border_mode='same', name='block1_conv1', subsample=(4,4)))(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
     x = TimeDistributed(MaxPooling2D((3, 3), strides=(2,2),name='block1_pool'))(x)
 
     # Block 2
-    x = TimeDistributed(Convolution2D(256, 10, 5, 5, border_mode='same', name='block2_conv1'))(input_tensor)
+    x = TimeDistributed(Convolution2D(256, 5, 5, border_mode='same', name='block2_conv1'))(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
     x = TimeDistributed(MaxPooling2D((3, 3), strides=(2,2),name='block2_pool'))(x)
     # Block 3
-    x = TimeDistributed(Convolution2D(384, 10, 3, 3, border_mode='same', name='block3_conv1'))(input_tensor)
+    x = TimeDistributed(Convolution2D(384, 3, 3, border_mode='same', name='block3_conv1'))(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
     # Block 3
-    x = TimeDistributed(Convolution2D(384, 10, 3, 3, border_mode='same', name='block4_conv1'))(input_tensor)
+    x = TimeDistributed(Convolution2D(384, 3, 3, border_mode='same', name='block4_conv1'))(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
     # Block 4
-    x = TimeDistributed(Convolution2D(256, 10, 3, 3, border_mode='same', name='block5_conv1'))(input_tensor)
+    x = TimeDistributed(Convolution2D(256, 3, 3, border_mode='same', name='block5_conv1'))(input_tensor)
     # x = BatchNormalization(axis=1)(x)
     x = Activation('relu')(x)
     x = TimeDistributed(MaxPooling2D((3, 3), strides=(2, 2), name='block5_pool'))(x)
 
-    x = TimeDistributed(Flatten(name='flatten'))(x)
-    x = Dropout(0.5)(x)
-    x = Dense(1024, W_regularizer=l2(0.01), name='fc1')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(256, W_regularizer=l2(0.01), name='fc2')(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-    x = Dropout(0.5)(x)
-    x = Dense(1, name='predictions')(x)
+    x = TimeDistributed(Flatten(name='flatten'), input_shape=(9, 3))(x)
+    #x = Dropout(0.5)(x)
+    #x = Dense(1024, W_regularizer=l2(0.01), name='fc1')(x)
+    #x = BatchNormalization()(x)
+    #x = Activation('relu')(x)
+    #x = Dropout(0.5)(x)
+    #x = Dense(256, W_regularizer=l2(0.01), name='fc2')(x)
+    #x = BatchNormalization()(x)
+    #x = Activation('relu')(x)
+    #x = Dropout(0.5)(x)
+    #x = Dense(1, name='predictions')(x)
     # x = BatchNormalization()(x)
     predictions = Activation('sigmoid')(x)
 

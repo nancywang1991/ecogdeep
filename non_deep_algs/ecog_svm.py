6952a35a110515803eedaabfc2530b7f83ac8c38
Nancy Wang <nancywang1991@yahoo.ca>
2017-05-04 20:31:29 -0700

    svm partial to whole

--- non_deep_algs/ecog_svm.py
+++ non_deep_algs/ecog_svm.py
@@ -15,80 +15,79 @@ for s, sbj in enumerate(sbj_ids):
         main_ecog_dir = '/home/wangnxr/dataset/ecog_vid_combined_%s_day%i/' % (sbj, days[s])
     else:
         continue
-    for itr in xrange(3):
-        for t, time in enumerate(start_times):
-            channels = channels_list[s]
-            ## Data generation
-            train_datagen_edf = EcogDataGenerator(
-                start_time=time,
-                time_shift_range=200,
-                gaussian_noise_range=0.001,
-                center=False,
-                fft = True
-            )
+    for t, time in enumerate(start_times):
+        channels = channels_list[s]
+        ## Data generation
+        train_datagen_edf = EcogDataGenerator(
+            start_time=time,
+            time_shift_range=200,
+            gaussian_noise_range=0.001,
+            center=False,
+            fft = True
+        )
 
-            test_datagen_edf = EcogDataGenerator(
-                start_time=time,
-                center=True,
-                time_shift_range = 200,
-                fft=True
-            )
+        test_datagen_edf = EcogDataGenerator(
+            start_time=time,
+            center=True,
+            time_shift_range = 200,
+            fft=True
+        )
 
-            dgdx_edf = train_datagen_edf.flow_from_directory(
-                # '/mnt/cb46fd46_5_no_offset/train/',
-                '%s/train/' % main_ecog_dir,
-                batch_size=24,
-                target_size=(1, len(channels), 1000),
-                final_size=(1, len(channels), 2),
-                class_mode='binary',
-                shuffle=False,
-                channels=channels,
-                pre_shuffle_ind=1)
+        dgdx_edf = train_datagen_edf.flow_from_directory(
+            # '/mnt/cb46fd46_5_no_offset/train/',
+            '%s/train/' % main_ecog_dir,
+            batch_size=24,
+            target_size=(1, len(channels), 1000),
+            final_size=(1, len(channels), 2),
+            class_mode='binary',
+            shuffle=False,
+            channels=channels,
+            pre_shuffle_ind=1)
 
-            dgdx_val_edf = test_datagen_edf.flow_from_directory(
-                # '/mnt/cb46fd46_5_no_offset/test/',
-                '%s/val/' % main_ecog_dir,
-                batch_size=10,
-                shuffle=False,
-                target_size=(1, len(channels), 1000),
-                final_size=(1, len(channels), 2),
-                channels=channels,
-                class_mode='binary')
+        dgdx_val_edf = test_datagen_edf.flow_from_directory(
+            # '/mnt/cb46fd46_5_no_offset/test/',
+            '%s/val/' % main_ecog_dir,
+            batch_size=10,
+            shuffle=False,
+            target_size=(1, len(channels), 1000),
+            final_size=(1, len(channels), 2),
+            channels=channels,
+            class_mode='binary')
 
-            train_generator=dgdx_edf
-            validation_generator=dgdx_val_edf
-            samples_per_epoch=train_generator.nb_sample
-            samples_per_epoch_test=validation_generator.nb_sample
-            x = []
-            y = []
-            for b in xrange(samples_per_epoch / train_generator.batch_size):
-                temp_data = train_generator.next()
-                x.append(temp_data[0])
-                y.append(temp_data[1])
-            x = np.vstack(x)
-            y = np.hstack(y)
-            print(x.shape)
-            print(y.shape)
+        train_generator=dgdx_edf
+        validation_generator=dgdx_val_edf
+        samples_per_epoch=train_generator.nb_sample
+        samples_per_epoch_test=validation_generator.nb_sample
+        x = []
+        y = []
+        for b in xrange(samples_per_epoch / train_generator.batch_size):
+            temp_data = train_generator.next()
+            x.append(temp_data[0])
+            y.append(temp_data[1])
+        x = np.vstack(x)
+        y = np.hstack(y)
+        print(x.shape)
+        print(y.shape)
 
-            val_x = []
-            val_y = []
-            for b in xrange(samples_per_epoch / validation_generator.batch_size):
-                temp_data = train_generator.next()
-                val_x.append(temp_data[0])
-                val_y.append(temp_data[1])
-            val_x = np.vstack(val_x)
-            val_y = np.hstack(val_y)
+        val_x = []
+        val_y = []
+        for b in xrange(samples_per_epoch / validation_generator.batch_size):
+            temp_data = train_generator.next()
+            val_x.append(temp_data[0])
+            val_y.append(temp_data[1])
+        val_x = np.vstack(val_x)
+        val_y = np.hstack(val_y)
 
-            model = SGDClassifier(verbose=0,n_jobs=8)
-            logfile = open("/home/wangnxr/history/ecog_model_svm_%s_itr_%i_t_%i.txt" % (sbj, itr, time), "wb")
-            #test_data = np.vstack([val for val in validation_generator])
-            best_score = 0
+        model = SGDClassifier(verbose=0,n_jobs=8)
+        logfile = open("/home/wangnxr/history/ecog_model_svm_%s_t_%i.txt" % (sbj, time), "wb")
+        #test_data = np.vstack([val for val in validation_generator])
+        best_score = 0
 
-            model.fit(np.reshape(x, (samples_per_epoch, x[0].shape[2]*x[0].shape[3])), y, classes=(0,1))
-            print("training acc: %f" % model.score(np.reshape(x, (samples_per_epoch, x[0].shape[2]*x[0].shape[3])),y), file=logfile)
-            print("validation acc: %f" % model.score(np.reshape(val_x, (samples_per_epoch, val_x[0].shape[2] * val_x[0].shape[3])), val_y),
-                file=logfile)
-            pickle.dump(model, open("/home/wangnxr/models/ecog_model_svm_%s_itr_%i_t_%i.p" % (sbj, itr, time), "wb"))
+        model.fit(np.reshape(x, (samples_per_epoch, x[0].shape[2]*x[0].shape[3])), y, classes=(0,1))
+        print("training acc: %f" % model.score(np.reshape(x, (samples_per_epoch, x[0].shape[2]*x[0].shape[3])),y), file=logfile)
+        print("validation acc: %f" % model.score(np.reshape(val_x, (samples_per_epoch, val_x[0].shape[2] * val_x[0].shape[3])), val_y),
+            file=logfile)
+        pickle.dump(model, open("/home/wangnxr/models/ecog_model_svm_%s_t_%i.p" % (sbj, time), "wb"))
 
 
 
--- non_deep_algs/ecog_svm_test.py
+++ non_deep_algs/ecog_svm_test.py
@@ -13,52 +13,51 @@ with open("/home/wangnxr/results/ecog_svm_summary_results.txt", "wb") as summary
     for s, sbj in enumerate(sbj_ids):
         for time in start_times:
             main_ecog_dir = '/home/wangnxr/dataset/ecog_vid_combined_%s_day%i/test/' % (sbj, days[s])
-            for itr in xrange(3):
-                model_files = glob.glob(
-                    '/home/wangnxr/models/ecog_model_svm_%s_itr_%i_t_%i.p' % (sbj, itr, time))
-                if len(model_files)==0:
-                    continue
-                ## Data generation ECoG
-                channels = channels_list[s]
-                model_file = model_files[0]
+            model_files = glob.glob(
+                '/home/wangnxr/models/ecog_model_svm_%s_t_%i.p' % (sbj, time))
+            if len(model_files)==0:
+                continue
+            ## Data generation ECoG
+            channels = channels_list[s]
+            model_file = model_files[0]
 
-                test_datagen = EcogDataGenerator(
-                        start_time=time,
-                        #f_lo=60,
-                        #f_hi=90,
-                        fft=True,
-                        center=False
-                )
+            test_datagen = EcogDataGenerator(
+                    start_time=time,
+                    #f_lo=60,
+                    #f_hi=90,
+                    fft=True,
+                    center=False
+            )
 
-                dgdx_val_edf = test_datagen.flow_from_directory(
-                        #'/mnt/cb46fd46_5_no_offset/test/',
-                        main_ecog_dir,
-                        batch_size=10,
-                        shuffle=False,
-                        target_size=(1,len(channels),1000),
-                        final_size=(1,len(channels),2),
-                        channels=channels,
-                        class_mode='binary')
+            dgdx_val_edf = test_datagen.flow_from_directory(
+                    #'/mnt/cb46fd46_5_no_offset/test/',
+                    main_ecog_dir,
+                    batch_size=10,
+                    shuffle=False,
+                    target_size=(1,len(channels),1000),
+                    final_size=(1,len(channels),2),
+                    channels=channels,
+                    class_mode='binary')
 
-                validation_generator =  dgdx_val_edf
-                samples_per_epoch_test = validation_generator.nb_sample
-                model = pickle.load(open(model_file))
-                mean_test_score_0 = []
-                mean_test_score_1 = []
-                for b in xrange(samples_per_epoch_test/validation_generator.batch_size):
-                        test_data = validation_generator.next()
-                        if len(test_data[0])<validation_generator.batch_size:
-                                validation_generator.reset()
-                                test_data = validation_generator.next()
-                        result = model.predict(np.reshape(test_data[0], (validation_generator.batch_size, test_data[0].shape[2]*test_data[0].shape[3])))
-                        for d, data in enumerate(test_data[1]):
-                                if data == 0:
-                                        mean_test_score_0.append(result[d])
-                                else:
-                                        mean_test_score_1.append(result[d])
-                summary_writer.write("%s\n" % model_file.split("/")[-1])
-                summary_writer.write("accuracy_0: %f\n" % (len(np.where(np.array(mean_test_score_0)==0)[0])/float(len(mean_test_score_0))))
-                summary_writer.write("accuracy_1: %f\n" % (len(np.where(np.array(mean_test_score_1) == 1)[0]) / float(len(mean_test_score_1))))
+            validation_generator =  dgdx_val_edf
+            samples_per_epoch_test = validation_generator.nb_sample
+            model = pickle.load(open(model_file))
+            mean_test_score_0 = []
+            mean_test_score_1 = []
+            for b in xrange(samples_per_epoch_test/validation_generator.batch_size):
+                    test_data = validation_generator.next()
+                    if len(test_data[0])<validation_generator.batch_size:
+                            validation_generator.reset()
+                            test_data = validation_generator.next()
+                    result = model.predict(np.reshape(test_data[0], (validation_generator.batch_size, test_data[0].shape[2]*test_data[0].shape[3])))
+                    for d, data in enumerate(test_data[1]):
+                            if data == 0:
+                                    mean_test_score_0.append(result[d])
+                            else:
+                                    mean_test_score_1.append(result[d])
+            summary_writer.write("%s\n" % model_file.split("/")[-1])
+            summary_writer.write("accuracy_0: %f\n" % (len(np.where(np.array(mean_test_score_0)==0)[0])/float(len(mean_test_score_0))))
+            summary_writer.write("accuracy_1: %f\n" % (len(np.where(np.array(mean_test_score_1) == 1)[0]) / float(len(mean_test_score_1))))
 
 
 

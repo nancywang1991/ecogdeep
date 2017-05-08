ca366e95755eb0d5577402e4231bfc4028e0f632
Nancy Wang <nancywang1991@yahoo.ca>
2017-05-08 15:08:26 -0700

    svm partial to whole

--- display/visualize_weights.py
+++ display/visualize_weights.py
@@ -3,13 +3,9 @@ import numpy as np
 from keras.models import load_model
 from scipy.misc import imsave
 import pdb
+from ecogdeep.train.sbj_parameters import *
+import glob
 
-model = load_model("/home/wangnxr/model_ecog_1d_1_3_1_2_mj.h5")
-layer_dict = dict([(layer.name, layer) for layer in model.layers])
-layer_name = "block1_conv1"
-filter_indexes = range(16)
-input_img = model.layers[2].input
-step=0.5
 
 def deprocess_image(x):
     # normalize tensor: center on 0., ensure std is 0.1
@@ -27,29 +23,41 @@ def deprocess_image(x):
     x = np.clip(x, 0, 255).astype('uint8')
     return x
 
-for filter_index in filter_indexes:
-	# build a loss function that maximizes the activation
-	# of the nth filter of the layer considered
-	layer_output = layer_dict[layer_name].output
-	loss = K.mean(layer_output[:, filter_index, :, :])
+for s, sbj in enumerate(sbj_ids):
+    for t, time in enumerate(start_times):
+        for itr in xrange(3):
+            model_files = glob.glob('/home/wangnxr/models/ecog_vid_model_lstm_%s_itr_%i_t_%i_*chkpt.h5' % (sbj, itr, time))
+            if len(model_files) == 0:
+                continue
+            model = load_model(model_files[0])
+            layer_dict = dict([(layer.name, layer) for layer in model.layers])
+            layer_name = "merge1"
+            input_img = model.input
+            step=0.5
 
-	# compute the gradient of the input picture wrt this loss
-	grads = K.gradients(loss, input_img)[0]
+            for filter_index in layer_dict[layer_name].output_shape[0]:
+                # build a loss function that maximizes the activation
+                # of the nth filter of the layer considered
+                layer_output = layer_dict[layer_name].output
+                loss = K.mean(layer_output[:, filter_index, :, :])
 
-	# normalization trick: we normalize the gradient
-	grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
+                # compute the gradient of the input picture wrt this loss
+                grads = K.gradients(loss, input_img)[0]
 
-	# this function returns the loss and grads given the input picture
-	iterate = K.function([input_img], [loss, grads])
+                # normalization trick: we normalize the gradient
+                grads /= (K.sqrt(K.mean(K.square(grads))) + 1e-5)
 
-	# we start from a gray image with some noise
-	input_img_data = np.random.random((1, 1, 64, 200)) * 20 + 128.
-	# run gradient ascent for 40 steps
-	for i in range(2000):
-    		loss_value, grads_value = iterate([input_img_data])
-    		input_img_data += grads_value * step
+                # this function returns the loss and grads given the input picture
+                iterate = K.function([input_img], [loss, grads])
+
+                # we start from a gray image with some noise
+                input_img_data = np.random.random((1, 1, 64, 200)) * 20 + 128.
+                # run gradient ascent for 40 steps
+                for i in range(2000):
+                        loss_value, grads_value = iterate([input_img_data])
+                        input_img_data += grads_value * step
 
 
-	img = input_img_data[0]
-	img = deprocess_image(img)
-	imsave('/home/wangnxr/results/weights/%s_filter_%d.png' % (layer_name, filter_index), img[:,:,0])
+                img = input_img_data[0]
+                img = deprocess_image(img)
+                imsave('/home/wangnxr/results/weights/%s_filter_%d.png' % (layer_name, filter_index), img[:,:,0])

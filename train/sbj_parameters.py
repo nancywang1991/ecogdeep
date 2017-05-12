b562e819ae3946ae137c79546370bf9622a65327
nancywang1991 <nancywang1991@yahoo.ca>
2017-05-12 00:58:17 -0700

    smth

--- train/sbj_parameters.py
+++ train/sbj_parameters.py
@@ -7,10 +7,6 @@ start_times = [2700, 3300, 3900]
 channels_list = [np.hstack([np.arange(36), np.arange(37, 65), np.arange(66, 92)]),
                  np.arange(80),
                  np.arange(82),
-<<<<<<< HEAD
 		 np.arange(80),
-=======
-		         np.arange(80),
->>>>>>> 3cd0685d61165433807c318e864dfed3749403e8
                  np.arange(86)]
 frames = [ range(3,8), range(5,10), range(7,12)]

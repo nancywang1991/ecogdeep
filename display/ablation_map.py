7227632df9ea00642ae8d9d451401e8bc8ceaf14
Nancy Wang <nancywang1991@yahoo.ca>
2017-06-02 14:08:16 -0700

    regression

--- display/ablation_map.py
+++ display/ablation_map.py
@@ -14,7 +14,6 @@ result_file = "C:/Users/Nancy/OneDrive/Documents/Documents/brunton_lab/NIPS2017/
 acc_orig = [0.64, 0.64,	0.65, 0.87,	0.88, 0.85, 0.69, 0.71,	0.71]
 
 results = open(result_file).readlines()
-#result start indexes for different experiments
 file_start_inds = [i for i in xrange(len(results)) if results[i][:4]=="ecog"]
 
 for i, ind in enumerate(file_start_inds):

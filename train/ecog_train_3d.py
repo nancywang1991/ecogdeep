14db570e9fa4605ceb6785ab3a1f43041342e100
Nancy Wang <nancywang1991@yahoo.ca>
2017-08-18 12:23:08 -0700

    3d train

--- train/ecog_train_3d.py
+++ train/ecog_train_3d.py
@@ -72,7 +72,7 @@ for s, sbj in enumerate(sbj_ids):
             validation_generator =  dgdx_val_edf
             base_model_ecog = Model(ecog_model.input, ecog_model.get_layer("fc1").output)
 
-            ecog_series = Input(shape=(1,len(channels),1000))
+            ecog_series = Input(shape=(1,8,8,1000))
 
             x = base_model_ecog(ecog_series)
 

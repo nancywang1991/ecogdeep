61d69e8fb9541740210a16ba0272914670f09074
Nancy Wang <nancywang1991@yahoo.ca>
2018-03-26 16:06:07 -0700

    toward away scheme for ecog movement regression

--- data/movement_ecog_data_toward_away_gen.py
+++ data/movement_ecog_data_toward_away_gen.py
@@ -25,13 +25,16 @@ if not os.path.exists(save_dir):
 
 types = ["train", "test", "val"]
 
-def calc_dist(a,b):
-    final_dist = []
-    if np.all(np.array(b) > -1000) and np.all(np.array(a) > -1000):
-        final_dist.append(np.sqrt((b[0]-a[0])**2 + (b[1]-a[1])**2))
+def calc_dist(a,b,t):
+    t2 = t
+    while np.all(b[t] == -1000):
+        t2-=1
+        if t2 <0:
+            return -1000
+    if np.all(np.array(b[t2]) > -1000) and np.all(np.array(a[t]) > -1000):
+        return np.sqrt((b[t2][0]-a[t][0])**2 + (b[t2][1]-a[t][1])**2)
     else:
-        final_dist.append(-1000)
-    return final_dist
+        return -1000
 
 
 for type in types:
@@ -44,7 +47,7 @@ for type in types:
             try:
                 crop_coords_prev = np.array([np.array([int(coord) for coord in crop_coord.split(',')]) for crop_coord in
                                              open(os.path.normpath("%s/%s_%s_%04i.txt" % (
-                                             crop_loc, sbj_id, day, int(vid) - 1))).readlines()])
+                                                 crop_loc, sbj_id, day, int(vid) - 1))).readlines()])
             except IOError:
                 print "Crop coords for %s not found" % (filename)
             poses = np.array([numerate_coords(row) for row in (open(joints_file_prev)).readlines()])
@@ -66,16 +69,16 @@ for type in types:
         poses_normalized_filtered = filter_confidence(poses_normalized, poses[:, :, 2])
         poses_normalized = my_savgol_filter(poses_normalized_filtered, 21, 3, axis=0)
         if file.split("/")[-2] == "r_arm_1":
-            start_dist = calc_dist(poses_normalized[(start + 4 * 30 + 15), 1], poses_normalized[(start + 4 * 30 + 15), 0])
-            end_dist = calc_dist(poses_normalized[-1, 1], poses_normalized[(-1), 0])
+            start_dist = calc_dist(poses_normalized[:, 1], poses_normalized[:, 0], start+4*30+15)
+            end_dist = calc_dist(poses_normalized[:, 1], poses_normalized[:, 0],-1)
         elif file.split("/")[-2] == "l_arm_1":
-            start_dist = calc_dist(poses_normalized[(start + 4 * 30 + 15), 2], poses_normalized[(start + 4 * 30 + 15), 0])
-            end_dist = calc_dist(poses_normalized[-1, 2], poses_normalized[-1, 0])
+            start_dist = calc_dist(poses_normalized[:, 2], poses_normalized[:, 0],  start+4*30+15)
+            end_dist = calc_dist(poses_normalized[:, 2], poses_normalized[:, 0], -1)
         else:
             print "Error: Not working with %s yet" % file.split("/")[-2]
             break
-	if end_dist > 0 and start_dist > 0:
-	    if end_dist < start_dist:
-            	shutil.copy(file, "%s/%s/toward/" % (save_dir, type))
+        if end_dist > 0 and start_dist > 0:
+            if end_dist < start_dist:
+                shutil.copy(file, "%s/%s/toward/" % (save_dir, type))
             elif end_dist > start_dist:
-            	shutil.copy(file, "%s/%s/away/" % (save_dir, type))
+                shutil.copy(file, "%s/%s/away/" % (save_dir, type))
